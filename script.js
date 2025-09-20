ff244df92974f39d5a1288d6f3817bc3ecbfbca0
technovatesahe <technovatesahe@gmail.com>
2025-09-20 20:27:23 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// This array will hold all your team members' data.
+// For now, we use dummy data. You will replace this later.
+const teamMembers = [
+    {
+        name: "Akash Sharma",
+        role: "President",
+        photoUrl: "", // Leave this empty for now
+        github: "https://github.com/akash",
+        linkedin: "https://linkedin.com/in/akash",
+        instagram: "https://instagram.com/akash"
+    },
+    {
+        name: "Priya Singh",
+        role: "Web Lead",
+        photoUrl: "", // Leave this empty
+        github: "https://github.com/priya",
+        linkedin: "https://linkedin.com/in/priya",
+        instagram: "https://instagram.com/priya"
+    },
+    {
+        name: "Rahul Kumar",
+        role: "Design Lead",
+        photoUrl: "", // Leave this empty
+        github: "https://github.com/rahul",
+        linkedin: "https://linkedin.com/in/rahul",
